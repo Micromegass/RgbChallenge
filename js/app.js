a82569f8b8ae01e319a43864dfedd94b4f1b043e
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-08-13 21:16:22 -0500

    change colors

---
+++ js/app.js
@@ -0,0 +1,23 @@
+
+
+var correct = Math.floor(Math.random() * 2)
+
+$('.option').each(function(index) {
+    var color = generateColor;
+    $(this).css('background-color', color);
+
+    if (index == correct) {
+        $('.question').text(color);
+    }
+});
+
+
+function generateColor() {
+    return 'rgb(' + random() + ', ' + random() + ', ' + random() + ')'; 
+}
+
+function random() {
+   return Math.floor(Math.random() * 255);
+}
+
+
